ec91026d36d02e5862b043a308c65872e04a00fe
lori <11811407@mail.sustech.edu.cn>
2024-01-25 23:08:19 -0800

    update dir

--- code-editor/index.js
+++ code-editor/index.js
@@ -1,11 +1,3 @@
-fetch("../js/dagger.js")
-    .then((res) => res.text())
-    .then((text) => {
-        // console.log(text)
-        // do something with "text"
-        window.dgjs = text
-    }).catch((e) => console.error(e));
-
 require.config({paths: {'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs'}});
 
 require(['vs/editor/editor.main'], function () {
@@ -60,7 +52,7 @@ require(['vs/editor/editor.main'], function () {
             "editor.foreground": "#000000",
         }
     });
-    // Create the editor with HTML language
+
     window.editor = monaco.editor.create(document.getElementById('container'), {
         value: ['<body>','<!-- Write your own code here -->', '</body>'].join('\n'),
         language: 'daggerJs',
@@ -71,65 +63,24 @@ require(['vs/editor/editor.main'], function () {
     });
 });
 
-function getCode() {
-    const header = '<html lang="en">\n' +
-    '<head>\n' +
-    '    <meta charset="utf-8"/>\n' +
-    '    <script\n' +
-    '            type="module"\n' +
-    '            crossorigin="anonymous"\n' +
-    '            src="../js/dagger.js"\n' +
-    '            defer\n' +
-    '    ></script>\n' +
-    '    <script type="dagger/modules"></script>\n' +
-    '    <title>dagger js</title>\n' +
-    '</head>'
-    return window.editor.getValue();
-    // return header + window.editor.getValue();
-    // return [
-    //     '<!DOCTYPE html>',
-    //     '<html>',
-    //     '<head>',
-    //     '    <title>Hello World</title>',
-    //     '</head>',
-    //     '<body +loading="{ a: []}">',
-    //     '    <h1>Hello, world!</h1>',
-    //     '</body>',
-    //     '</html>'
-    // ].join('\n')
-}
-
-
 
 function updateIframe() {
     document.getElementById('htmlOutput').src = getGeneratedPageURL({
-        html: getCode(),
-        css: ""
+        html: window.editor.getValue(0)
     })
-    // var htmlCode = getCode();
-    // var iframe = document.getElementById('htmlOutput');
-    // var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    //
-    // iframeDoc.open();
-    // iframeDoc.write(htmlCode);
-    // iframeDoc.close();
 }
 
-// document.getElementById('htmlInput').addEventListener('input', updateIframe);
-
-const getGeneratedPageURL = ({ html, css }) => {
+const getGeneratedPageURL = ({ html }) => {
     const getBlobURL = (code, type) => {
         const blob = new Blob([code], { type })
         return URL.createObjectURL(blob)
     }
 
-    const cssURL = getBlobURL(css, 'text/css')
     const jsURL = getBlobURL(window.dgjs, 'text/javascript')
 
     const source = `
     <html>
       <head>
-        ${css && `<link rel="stylesheet" type="text/css" href="${cssURL}" />`}
         <script type="module" crossorigin="anonymous" src="${jsURL}" defer></script><script type="dagger/modules"></script>
       </head>
       <body>
