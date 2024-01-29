981891778bb454623b1885249700da8b674c048d
miyi <mylegojesus@gmail.com>
2024-01-29 00:07:37 -0800

    pure cdn monaco editor project folder

--- monaco-import-from-cdn/dist/script.js
+++ monaco-import-from-cdn/dist/script.js
@@ -1,36 +1,31 @@
-import * as monaco from 'https://esm.sh/monaco-editor';
-import editorWorker from 'https://esm.sh/monaco-editor/esm/vs/editor/editor.worker?worker';
-import jsonWorker from 'https://esm.sh/monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'https://esm.sh/monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'https://esm.sh/monaco-editor/esm/vs/language/html/html.worker?worker';
+import * as monaco from "https://esm.sh/monaco-editor";
+import editorWorker from "https://esm.sh/monaco-editor/esm/vs/editor/editor.worker?worker";
+import jsonWorker from "https://esm.sh/monaco-editor/esm/vs/language/json/json.worker?worker";
+import cssWorker from "https://esm.sh/monaco-editor/esm/vs/language/css/css.worker?worker";
+import htmlWorker from "https://esm.sh/monaco-editor/esm/vs/language/html/html.worker?worker";
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
-    if (label === 'json') {
+    if (label === "json") {
       return new jsonWorker();
     }
-    if (label === 'css' || label === 'scss' || label === 'less') {
+    if (label === "css" || label === "scss" || label === "less") {
       return new cssWorker();
     }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+    if (label === "html" || label === "handlebars" || label === "razor") {
       return new htmlWorker();
     }
     return new editorWorker();
-  }
+  },
 };
 
-monaco.editor.create(document.getElementById('container'), {
-  value: "function hello() {\n\talert('Hello world!');\n}",
-  language: 'javascript'
-});
+export const editor = monaco.editor;
 
-export * as monaco from 'https://esm.sh/monaco-editor';
-
-export const initMonaco = (monaco, $node) => {
+export const initMonaco = (editor, $node) => {
   if (monaco) {
-    monaco.editor.create($node, {
+    editor.create($node, {
       value: "function hello() {\n\talert('Hello world!');\n}",
-      language: 'javascript'
+      language: "javascript",
     });
   }
-}
+};
