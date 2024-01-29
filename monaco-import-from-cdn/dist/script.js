ba61a4977052b136113ad5943dfed9492dc4e8f6
miyi <mylegojesus@gmail.com>
2024-01-29 01:23:28 -0800

    integrated code-editor into the esm-import project

--- monaco-import-from-cdn/dist/script.js
+++ monaco-import-from-cdn/dist/script.js
@@ -19,13 +19,43 @@ self.MonacoEnvironment = {
   },
 };
 
-export const editor = monaco.editor;
+export const monacoEditor = monaco.editor;
 
-export const initMonaco = (editor, $node) => {
+export const initMonaco = (monaco, $node) => {
   if (monaco) {
-    editor.create($node, {
-      value: "function hello() {\n\talert('Hello world!');\n}",
-      language: "javascript",
+    return monaco.create($node, {
+      value: [
+        "<body>",
+        `     <div class="card" +loading="{a: '123', b: 1}" $watch="b++">`,
+        "             ${a}",
+        "             ${b}",
+        `     </div>`,
+        "</body>",
+      ].join("\n"),
+      language: "html",
+      minimap: {
+        enabled: false,
+      },
     });
   }
+};
+
+export const getGeneratedPageURL = ( html ) => {
+  const getBlobURL = (code, type) => {
+    const blob = new Blob([code], { type });
+    return URL.createObjectURL(blob);
+  };
+
+  const source = `
+    <html>
+      <head>
+        <script type="module" crossorigin="anonymous" src="https://cdn.jsdelivr.net/npm/@miyi/dagger.js" defer></script><script type="dagger/modules"></script>
+      </head>
+      <body>
+        ${html || ""}
+      </body>
+    </html>
+  `;
+
+  return getBlobURL(source, "text/html");
 };
